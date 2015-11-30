cd76fc37ea93421d30af9559a8747c54a155f8e7
NateFuller <fatenuller@gmail.com>
2015-11-29 23:10:46 -0500

    Styling, color scheme (not sure I like it)

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
+  res.locals.view_home = true;
   res.render('home', { title: "Kill the Mutants" });
 });
 
 router.get('/about', (req, res) => {
+  res.locals.view_about = true;
   res.render('about', { title: "About Kill the Mutants" });
 });
 
 router.get('/login', (req, res) => {
-  res.render('/oauth/login', { title: "Login to KTM" });
+  res.redirect('/oauth/login');
 });
 
 module.exports = router;
