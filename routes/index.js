1606c6d3d927d6235b39abe15db06f26bc755626
Ryan Stanley <stanleyrya@gmail.com>
2015-12-01 17:47:30 -0500

    cleanup; now calls login logic

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', function(req, res) {
   res.locals.view_home = true;
   res.render('home', { title: "Kill the Mutants" });
 });
 
-router.get('/about', (req, res) => {
+router.get('/about', function(req, res) {
   res.locals.view_about = true;
   res.render('about', { title: "About Kill the Mutants" });
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', function(req, res) {
   res.redirect('/oauth/login');
 });
 
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var github = require('../lib/github');
+var users = require('../lib/users');
 var tokens = require('../lib/api.json');
 var querystring = require('querystring');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/callback', (req, res) => {
+router.get('/login', function(req, res) {
+  var hostname = req.headers.host; // hostname = 'localhost:8080'
+  var uri = 'http://'+hostname+'/oauth/callback'; // pathname = '/MyApp'
+
+  var query = {
+    client_id : tokens.GITHUB_CLIENT_ID,
+    redirect_uri : uri,
+    scope : 'user:email,public_repo'
+  };
+
+  res.redirect('https://github.com/login/oauth/authorize?' + querystring.stringify(query));
+});
+
+router.get('/callback', function(req, res) {
   var error = req.query.error;
   if(error) {
     res.render('500', { title : error });
@@ -13,28 +26,20 @@ router.get('/callback', (req, res) => {
 
   var hostname = req.headers.host; // hostname = 'localhost:8080'
   var uri = 'http://'+hostname+'/oauth/callback'; // pathname = '/MyApp'
-
   var code = req.query.code;
+
   var authenticationInformation = {
     code: code,
     redirect_uri: uri
   };
+
   github.requestToken(authenticationInformation, function(data) {
-    res.cookie('access_token', data.access_token);
-    res.redirect('/');
+    var access_token = data.access_token;
+    users.login(access_token, function() {
+      res.cookie('access_token', data.access_token);
+      res.redirect('/');
+    });
   });
-});
-
-router.get('/login', (req, res) => {
-  var hostname = req.headers.host; // hostname = 'localhost:8080'
-  var uri = 'http://'+hostname+'/oauth/callback'; // pathname = '/MyApp'
-
-  var query = {
-    client_id : tokens.GITHUB_CLIENT_ID,
-    redirect_uri : uri,
-  };
-
-  res.redirect('https://github.com/login/oauth/authorize?' + querystring.stringify(query));
 });
 
 module.exports = router;
