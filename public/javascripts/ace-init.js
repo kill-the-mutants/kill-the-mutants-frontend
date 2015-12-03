badd4611a8827e8c3fa62249916d076151dc7d0a
NateFuller <fatenuller@gmail.com>
2015-12-03 05:04:00 -0500

    Styling

--- public/javascripts/ace-init.js
+++ public/javascripts/ace-init.js
@@ -1,3 +1,17 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/cobalt");
 editor.getSession().setMode("ace/mode/java");
+editor.setOptions({
+  maxLines: Infinity
+});
+
+var snippetView = ace.edit("code-snippet");
+snippetView.setTheme("ace/theme/cobalt");
+snippetView.getSession().setMode("ace/mode/java");
+snippetView.setOptions({
+    maxLines: Infinity,
+    readOnly: true,
+    highlightActiveLine: false,
+    highlightGutterLine: false
+});
+snippetView.renderer.$cursorLayer.element.style.opacity=0;
