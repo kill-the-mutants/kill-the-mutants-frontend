60a0cf36e4cd383242f471ebe2a2fffbcade2091
NateFuller <fatenuller@gmail.com>
2015-12-04 03:56:34 -0500

    Styled the game page

--- public/javascripts/ace-init.js
+++ public/javascripts/ace-init.js
@@ -2,14 +2,14 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/cobalt");
 editor.getSession().setMode("ace/mode/java");
 editor.setOptions({
-  maxLines: Infinity
+  maxLines: 25
 });
 
 var snippetView = ace.edit("code-snippet");
 snippetView.setTheme("ace/theme/cobalt");
 snippetView.getSession().setMode("ace/mode/java");
 snippetView.setOptions({
-    maxLines: Infinity,
+    maxLines: 25,
     readOnly: true,
     highlightActiveLine: false,
     highlightGutterLine: false
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,1 +1,14 @@
-// TODO: this...
+$(document).ready(function() {
+  $('#btn-compile').click(function() {
+    console.log('Compiling code...');
+    console.log('Done!');
+  });
+  $('#btn-run').click(function() {
+    console.log('Running tests...');
+    console.log('Done!');
+  });
+  $('#btn-kill-mutants').click(function() {
+    console.log('Killing mutants...');
+    console.log('Done!');
+  });
+});
